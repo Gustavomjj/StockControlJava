c60bbeca023cb9c4358d2f8f2cd0235829367d4e
Gustavo Maciel Jacinto <126967183+Gustavomjj@users.noreply.github.com>
2023-06-02 14:20:21 -0300

    Add files via upload

--- Product.java
+++ Product.java
@@ -2,10 +2,44 @@ package projetos.controleEstoque;
 
 public class Product {
 	
-	String name;
-	double price;
-	int quantity;
+	private String name;
+	private double price;
+	private int quantity;
 	
+	public Product() {
+	}
+	
+	public Product(String name, double price, int quantity) {
+		this.name = name;
+		this.price = price;
+		this.quantity = quantity;
+	}
+	
+	public Product(String name, double price) {
+		this.name = name;
+		this.price = price;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
 	double totalValueInStock() {
 		double total = price * quantity;
 		return total;
@@ -30,5 +64,11 @@ public class Product {
 				 + String.format("R$%.2f\n", totalValueInStock())
 				 + "-----";
 	}
+	 
+	 static void limpaTela() {
+			for (int i = 0; i < 3; i++) {
+			System.out.println();	
+			}
+	 }
 	
 }
--- sistema.java
+++ sistema.java
@@ -1,5 +1,7 @@
 package projetos.controleEstoque;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class sistema {
@@ -7,43 +9,121 @@ public class sistema {
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
+		List<Product> produtos = new ArrayList<>();
 		
-		Product p1 = new Product(); 
-		System.out.println("Entre os dados do produto:");
-		System.out.println("Nome: ");
-		p1.name = sc.nextLine();
+		String opcao = "";
 		
-		System.out.println("Preço:");
-		p1.price = sc.nextDouble();
-		
-		System.out.println("Quantidade no estoque:");
-		p1.quantity = sc.nextInt();
-				
-		System.out.println(p1);
-		
-		String saida = "";
-		
-		while (!saida.equalsIgnoreCase("sim")) {
+		while (!opcao.equalsIgnoreCase("sair")) {
+			System.out.println("------------------------------");
+			System.out.println("----- CONTROLE DE ESTOQUE ----");
+			System.out.println("------------ MENU ------------");
+			System.out.println("1. Adicionar produto");
+			System.out.println("2. Adicionar quantidade ao estoque");
+			System.out.println("3. Retirar uma quantidade do estoque");
+			System.out.println("4. Ver lista de produtos");
+			System.out.println("Digite 'sair' para encerrar o programa");
+			System.out.println("------------------------------");
+			System.out.println("Digite uma opção: ");
+			opcao = sc.nextLine();
 			
-		System.out.println("Digite o número de produtos a ser adicionado no estoque: ");
-		p1.addProducts(sc.nextInt());
-		
-		System.out.println(p1);
-						
-		System.out.println("Entre o número de produtos a ser removido do estoque: ");
-		p1.removeProduct(sc.nextInt());
-		
-		System.out.println(p1); 
-		
-		System.out.println("Deseja sair (Sim / Não): ");
-		saida = sc.next();
+			
+			switch (opcao) {
+				case "1":
+					System.out.println("Entre os dados do produto: ");
+					System.out.println("Nome: ");
+					String name = sc.nextLine();
+					
+					System.out.println("Preço: ");
+					double price = sc.nextDouble();
+					sc.nextLine();
+					Product novoProduto = new Product(name, price);
+					
+					System.out.println("Quantidade no estoque: ");
+					int quantidade = sc.nextInt();
+					sc.nextLine();
+					novoProduto.addProducts(quantidade);
+					
+					produtos.add(novoProduto);
+					System.out.println("Produto adicionado com sucesso!");
+					Product.limpaTela();
+					break;
+					
+
+				case "2": 
+					System.out.println("Digite o nome do produto: ");
+					String nomeProduto = sc.nextLine();
+					boolean encontrado = false;
+					
+					for (Product produto : produtos) {
+						if (produto.getName().equalsIgnoreCase(nomeProduto)) {
+							System.out.println("Digite a quantidade a ser adicionada: ");
+							quantidade = sc.nextInt();
+							sc.nextLine();
+							produto.addProducts(quantidade);
+							System.out.println("Quantidade adicionada com sucesso.");
+							encontrado = true;
+							Product.limpaTela();
+							break;						
+						}
+					}
+					
+					if (!encontrado) {
+						System.out.println("Produto não encontrado.");
+						Product.limpaTela();
+					}
+					break;
+					
+				case "3": 
+					System.out.println("Digite o nome do produto: ");
+					nomeProduto = sc.nextLine();
+					encontrado = false;
+					
+					for (Product produto : produtos) {
+						if (produto.getName().equalsIgnoreCase(nomeProduto)) {
+							System.out.println("Digite a quantidade a ser retirada: ");
+							quantidade = sc.nextInt();
+							sc.nextLine();
+							produto.removeProduct(quantidade);
+							System.out.println("Quantidade retirada com sucesso.");
+							encontrado = true;
+							Product.limpaTela();
+							break;
+						}
+					}
+					
+					if (!encontrado) {
+						System.out.println("Produto não encontrado.");
+						Product.limpaTela();
+					}
+					break;
+					
+				case "4": 
+					
+					System.out.println("Lista de produtos:");
+					for (Product produto: produtos) {
+						System.out.println(produto);
+					}
+					Product.limpaTela();
+					break;
+					
+				case "Sair": 
+					break;
+					
+				default: 
+					System.out.println("Opção invalida. Por favor, escolha uma opção válida: ");
+					Product.limpaTela();
+					break;
+					
+			}
+					
 		}
 		
-		System.out.println("Sistema finalizado com sucesso!");
-				
-		sc.close();
-
+			System.out.println("Sistema finalizado com sucesso.");
+			
+			sc.close();
+		}		
+			
 	}
+	
 
-}
 			
